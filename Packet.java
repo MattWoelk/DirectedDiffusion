f78d462bb563ea8081b4382d7cd8615e5231a5cd
Matthew Woelk <matt.woelk@gmail.com>
2012-02-14 21:21:58 -0600

    Added RacketReceiver code and modifications to it.

--- Packet.java
+++ Packet.java
@@ -1,6 +1,31 @@
 public class Packet
 {
-  public Packet()
+  private char packetType;
+  private String senderID;
+  private double seqNumber;
+  private Date interval;
+
+  public Packet(char packetType, String senderID, double seqNumber, Date interval)
   {
+    this.packetType = packetType;
+    this.senderID = senderID;
+    this.seqNumber = seqNumber;
+    this.interval = interval;
+  }
+
+  public char getPacketType(){
+    return packetType;
+  }
+
+  public String getSenderID(){
+    return senderID;
+  }
+
+  public double getSeqNum(){
+    return seqNumber;
+  }
+
+  public Date getInterval(){
+    return interval;
   }
 }
--- PacketReceiver.java
+++ PacketReceiver.java
@@ -1,14 +1,95 @@
+import java.util.ArrayList;
+import java.sql.Date;
+
 public class PacketReceiver
 {
-  public PacketReceiver()
+  ArrayList<Packet> myRecPac;
+
+  public  PacketReceiver()
   {
+    myRecPac = new ArrayList<Packet>();
   }
 
-  public void receivePacket(Packet pkt)
+  public void populatereceiver(Nodes ob)
   {
+    for(Packet a:ob.getpacinnode())
+    {
+      myRecPac.add(a);
+    }
+  }
+
+  public ArrayList<Packet> getReceiver_transaction()
+  {
+    return myRecPac;
+  }
+
+  public void remove_trans()
+  {
+    myRecPac.clear();
+  }
+
+  public boolean check_empty()
+  {
+    return myRecPac.isEmpty();
   }
 
   public Packet getPacket()
   {
+    return myRecPac.remove(myRecPac.size()-1);
+  }
+
+  public void receivePacket(Packet pkt)
+  {
+    myRecPac.add(pkt);
+  }
+}
+
+class Nodes
+{
+    ArrayList<Packet> pac_in_node;
+    public Nodes()
+    {
+      pac_in_node = new ArrayList <Packet>();
+    }
+
+    public ArrayList<Packet> getpacinnode()
+    {
+      return pac_in_node;
+    }
+}
+
+class ReceivedPacket
+{
+  ArrayList<Packet> Rec_pac;
+  public ReceivedPacket()
+  {
+    Rec_pac=new ArrayList<Packet>();
+
+  }
+
+  public void populatestorage(PacketReceiver ob)
+  {
+
+    if (ob.check_empty())
+    {
+      ob.remove_trans();
+    }
+    else
+    {
+      for(Packet a:ob.getReceiver_transaction())
+      {
+
+        Rec_pac.add(a);
+      }
+
+      ob.remove_trans();
+
+    }
+  }
+
+
+  public ArrayList<Packet> getStorage_transaction()
+  {
+    return Rec_pac;
   }
 }
