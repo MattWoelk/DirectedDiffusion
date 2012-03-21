afae81b610a12ccfc044082a6f3ffd5c53c80e30
Matthew Woelk <matt.woelk@gmail.com>
2012-03-21 15:15:15 -0500

    better naming for output files

--- RUNTESTS.sh
+++ RUNTESTS.sh
@@ -2,6 +2,6 @@ for rng in 1 2 3 4 5
 do
   for dim in 10 15 20
   do
-    javac NodeTest.java && java NodeTest --filename-last-only=data/SIMHDA_dim="$dim"_rng="$rng"_"$(date +%s)".csv --dimension="$dim" --numNodes=55 --range=$rng --suppressOutput
+    javac NodeTest.java && java NodeTest --filename-last-only=data/SIMHDA_"$(date +%s)"_dim="$dim"_rng="$rng".csv --dimension="$dim" --numNodes=55 --range=$rng --suppressOutput
   done
 done
